da84c51a1b6c29fcae4f1ce2ccbd056bfd6a6246
Patrick <Patrick@nytyler.com>
2024-12-04 09:53:04 -0500

    Hazards updated

--- src/Hazards.hs
+++ src/Hazards.hs
@@ -1,15 +1,25 @@
-import Types(Sense(Feel, Hear), Hazard(Pit, Bats), Position, CaveLayout )
-
+module Hazards where
+import Types (Hazard(..), Sense(..), Position, CaveLayout, Move(..), GameState(..), PlayerState(..))
 
 toSense :: Hazard -> Sense
 toSense Bats = Hear
 toSense Pit  = Feel
 
-senseHazards :: Position -> CaveLayout -> [(Position, Hazard)] -> [Sense]
-senseHazards position layout hazards =
-    let neighbors = layout !! position
-        nearbyHazards = filter (\(pos, _) -> pos `elem` neighbors) hazards
-    in map (toSense . snd) nearbyHazards
+
+senseHazards :: GameState -> [Sense]
+senseHazards gameState = do
+    -- maybe there should be a better way
+    let movementOptions = [Types.Left, Types.Right, Types.Back];
+    let currentPos = currentPosition (playerState gameState)
+    let lastPos = lastPosition (playerState gameState)
+    let moveInMap = mover gameState
+    let neighbors = map (\move -> moveInMap currentPos lastPos move) movementOptions
+    let senses = if wumpusPosition (wumpusState gameState) `elem` neighbors then [Smell] else []
+    return senses
+
+    -- let neighbors = layout !! position
+    --     nearbyHazards = filter (\(pos, _) -> pos `elem` neighbors) hazards
+    -- in map (toSense . snd) nearbyHazards
 
 
 handleHazards :: Position -> [(Position, Hazard)] -> Maybe String
@@ -18,5 +28,3 @@ handleHazards position hazards =
         Just Bats -> Just "A swarm of super-bats swoops in and lifts you away."
         Just Pit  -> Just "You fell into a bottomless pit and died."
         Nothing   -> Nothing
-
-    
