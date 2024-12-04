9d5910d76565064ee576c157cf416c317c02ef53
JoshuaChenoweth <123035210+JoshuaChenoweth@users.noreply.github.com>
2024-12-04 10:11:57 -0500

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,7 @@ import Lib
 import Types
 import System.Random(mkStdGen)
 
-<<<<<<< HEAD
+
 main :: IO()
 main = do
     let gen = mkStdGen 27
@@ -12,12 +12,11 @@ main = do
     let enviornment = initializeEnvironment newGen
     print ws
     print enviornment
-=======
+
 main :: IO ()
 main = do
     initialState <- initializeGameState 27
     gameLoop initialState
->>>>>>> 744ce6c9946fd13de3fa566765eca47bca5a2c40
 
 
 
