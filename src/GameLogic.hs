e71fb930d1f5b7bf5c50f6954a5b32e026d2d0a6
nlongo1004 <112398240+nlongo1004@users.noreply.github.com>
2024-12-09 17:16:15 -0500

    so i can pull

--- src/GameLogic.hs
+++ src/GameLogic.hs
@@ -66,16 +66,15 @@ data StartGameState = StartGameState
 createStartState :: StartGameState -> GameState
 createStartState startGameState = GameState
     { 
-      playerState = PlayerState { 
-          currentPosition = playerCurrentPosition startGameState,
-          lastPosition = playerLastPostion startGameState,
-          arrowCount = playerArrowCount startGameState
-      },
+        playerState = PlayerState { 
+            currentPosition = playerCurrentPosition startGameState,
+            lastPosition = playerLastPostion startGameState,
+            arrowCount = playerArrowCount startGameState
+        },
       wumpusState = WumpusState { wumpusPosition = wumpusPos },
       environmentState = EnvironmentState { hazards = envHazards },
       randomGen = nextGen,
-      mover = move (caveLayout startGameState),
-      isAlive = True
+      mover = move (caveLayout startGameState)
     }
     where 
         indices = [0..(length (caveLayout startGameState)-1)]
@@ -119,4 +118,10 @@ updateArrowCount gameState =
                 arrowCount = updatedArrows 
             } 
         }
+
+
+
+
+
+
 
